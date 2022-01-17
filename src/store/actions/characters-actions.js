68e0ad51af6522151f5988b22caf709b9924e684
tarnovskii <artem3dgame@gmail.com>
2022-01-17 20:18:47 +0200

    fix

--- src/store/actions/characters-actions.js
+++ src/store/actions/characters-actions.js
@@ -4,7 +4,7 @@ export const fetchAllCharacters = () => {
     return dispatch => {
         dispatch({type: '[CHARACTERS] FETCH_ALL_CHARACTERS_STARTED'})
 
-        new StarWarsEntityApi().getAllEntitiesList([], 'https://swapi.dev/api/people/?page=7').then(result => {
+        new StarWarsEntityApi().getAllEntitiesList([], 'https://swapi.dev/api/people/?page=1').then(result => {
             dispatch({type: '[CHARACTERS] FETCH_ALL_CHARACTERS_SUCCESS', value: result})
         }).catch(error => {
             dispatch({type: '[CHARACTERS] FETCH_ALL_CHARACTERS_FAIL', value: error})
